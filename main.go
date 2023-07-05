fc5ec964242f38ff569df34f1c4649940a080e06
Pannathon Saman <pannathon@idpassglobal.com>
2023-07-05 09:50:05 +0700

    server 8080

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("https://github.com/Pannathon-idPass/GO.git")
+
+	http.HandleFunc("/", start)
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func start(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Start")
+
 }
